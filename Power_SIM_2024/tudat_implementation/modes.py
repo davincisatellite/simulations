b15e0579026a307a6a6ab5c9443c6afb08c3de90
TomasQReis <tomas.qreis@gmail.com>
2026-02-10 13:25:43 +0100

    Removed old modes code
    - Old modes class implementation

--- Power_SIM_2024/tudat_implementation/modes.py
+++ Power_SIM_2024/tudat_implementation/modes.py
@@ -84,7 +84,6 @@ class mode:
         else:
             return False
 
-    # TODO: Problem with current implementation is two modes can be active at the same time.
     def check_active(self,
                      batteryCharge: float,
                      sunlit: float,
@@ -96,7 +95,6 @@ class mode:
         """
 
         # Initializes an array of booleans.
-        # TODO: This can be made into some variable that actually tells you what condition/s are failing.
         activeChecklist = [False] * 3
 
         activeChecklist[0] = self.check_runtime(currentTime= currentTime)
@@ -122,101 +120,6 @@ class mode:
     def force_active(self):
         # Sets mode to active. Used in verification.
         self.isActive = True
-
-
-# TODO: These are all old implementations. Delete when new ones work properly.
-class active_mode: 
-    """Mode class.
-    Args:
-    - batteryCond: battery charge condition for mode to be active.
-    - sunlightCond: sunlit condition for mode to be active.
-    - powerDrain: float. Power drain of mode in W. 
-    """
-    def __init__(self, modeName:str, batteryCond=0.0 , sunlightCond=False,
-                 powerDrain=0.0, switchTo:str="safe"):
-        
-        self.name = modeName
-        self.batteryCond = batteryCond
-        self.sunlightCond = sunlightCond
-        self.powerDrain = powerDrain
-        self.switchTo = switchTo
-
-    def check_run(self, batteryCharge, sunlight):
-        """Checks if conditions for mode to be active are met.
-
-        Args:
-        - batteryCharge: float W*h, current battery charge. 
-        - sunglight: 1-0 depending on sunglight exposure. (1 = sunlit)
-        Returns:
-        - modeRuns: Boolean. True if running conditions are met. 
-        """
-        if self.batteryCond:
-            if batteryCharge > self.batteryCond:
-                batteryCheck = True
-            else: 
-                batteryCheck = False
-                # Temporary log statement. 
-                print(f"{self.name} mode switched due to low power.")
-        else:
-            batteryCheck = True
-            
-        if self.sunlightCond:
-            if sunlight >= self.sunlightCond:
-                sunlightCheck = True
-            else:
-                sunlightCheck = False
-                # Temporary log statement. 
-                print(f"{self.name} mode switched due to eclipse.")
-        else:
-            sunlightCheck = True
-
-        checkRun = batteryCheck and sunlightCheck
-
-        return checkRun, safeKey
-    
-
-class safe_mode: 
-    """safe mode class. Defines the safe mode and conditions for switching to
-    other (Active) modes.
-    Args:
-    - batteryCond: 
-    - sunlightCond: 
-    - powerDrain: float. Power drain of mode in W. 
-    """
-    def __init__(self, modeName:str, batteryCond=0.0, sunlightCond=False,
-                 powerDrain=0.0):
-        
-        self.name = modeName
-        self.batteryCond = batteryCond
-        self.sunlightCond = sunlightCond
-        self.powerDrain = powerDrain
-
-    def switch_to_active(self, batteryCharge):
-        """Switches to active mode. Placeholder. """
-        if batteryCharge > self.batteryCond:
-            batteryCheck = False
-            print(f"{self.name} mode switched due to enough power.")
-        else: 
-            batteryCheck = True
-
-        return batteryCheck, activeKey
-
-    def check_run(self, batteryCharge, sunlight):
-        """Checks if conditions for mode to be active are met. Currently built
-        to accomodate a series of modes to switch to.
-
-        Args:
-        - batteryCharge: float W*h, current battery charge. 
-        - sunglight: 1-0 depending on sunglight exposure. (1 = sunlit)
-        Returns:
-        - modeRuns: Boolean. True if running conditions are met. 
-        """
-        
-        checkRun, switchMode = self.switch_to_active(
-            batteryCharge= batteryCharge)
-
-        return checkRun, switchMode
-    
             
 # TODO: This needs a citation. Is it the safe mode consumption??
 powerPassiveBase = 0.66           # W
